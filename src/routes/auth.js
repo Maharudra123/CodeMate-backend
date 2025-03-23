1e1356822511dd31667f03996531842e82f61c90
yash bhidawe <ybh5179@gmail.com>
2025-03-23 18:41:15 +0530

    :fix: sameSite

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -45,7 +45,9 @@ authRouter.post("/login", async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+      secure: true,
+      sameSite: "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
     });
     res.send(user);
   } catch (error) {
