ee633e20cc1f1be12049ea5117a3bce1e9c3a9b1
yash bhidawe <ybh5179@gmail.com>
2025-03-23 18:51:02 +0530

    fix: sameSite: none

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -46,7 +46,7 @@ authRouter.post("/login", async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: true,
-      sameSite: "strict",
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
     res.send(user);
