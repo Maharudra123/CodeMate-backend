6750698e31f3dc17aeb0d5006e642a0ceca7f3c1
yash bhidawe <ybh5179@gmail.com>
2025-04-16 13:10:06 +0530

    logout issue

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -57,10 +57,11 @@ authRouter.post("/login", async (req, res) => {
 
 authRouter.post("/logout", async (req, res) => {
   res
-    .cookie("token", null, {
-      expires: new Date(Date.now()),
+    .clearCookie("token", {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
     })
     .send("logged out successfully");
 });
-
 module.exports = authRouter;
