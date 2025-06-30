8b29d711c3413c786efa1781dd0a13e919a8144f
Maharudra Shivraj Ganjure <102021400+Maharudra123@users.noreply.github.com>
2025-06-30 13:34:52 +0530

    Update app.js
    add frontend link in cors setting

--- src/app.js
+++ src/app.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://tinder-for-geeks.vercel.app"],
+    origin: ["http://localhost:5173", "https://code-mate-frontend-alpha.vercel.app/landing"],
     credentials: true,
   })
 );
